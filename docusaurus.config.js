435908428873d9e4e1ea5541e8e46179f96fafb6
kuaimeizheng <kuaimeizheng@gmail.com>
2022-06-01 18:28:01 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
   url: 'https://kuaimeizheng.github.io',
-  baseUrl: '/',
+  baseUrl: '/OrigamiStudio/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
