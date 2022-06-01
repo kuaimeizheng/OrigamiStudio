aa3b09e595a549d71096433eaafcbf86143e16bd
kuaimeizheng <kuaimeizheng@gmail.com>
2022-06-02 01:34:09 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -40,7 +40,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/kuaimeizheng/OrigamiStudio/tree/main/docs',
         },
         blog: {
           showReadingTime: true,
