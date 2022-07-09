62ce72e38fbb74f29a32612f05251352c10f633a
kuaimeizheng <kuaimeizheng@gmail.com>
2022-07-09 19:06:46 +0800

    添加了内容

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,186 +6,188 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-    title: 'Origami 研习社',
-    tagline: 'Origami 中文文档&使用手册&教程',
-    url: 'https://kuaimeizheng.github.io',
-    baseUrl: '/OrigamiStudio/',
-    onBrokenLinks: 'throw',
-    onBrokenMarkdownLinks: 'warn',
-    favicon: 'img/favicon.ico',
+  title: 'Origami 研习社',
+  tagline: 'Origami 中文文档&使用手册&教程',
+  url: 'https://kuaimeizheng.github.io',
+  baseUrl: '/OrigamiStudio/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
 
-    // GitHub pages deployment config.
-    // If you aren't using GitHub pages, you don't need these.
-    organizationName: 'kuaimeizheng', // Usually your GitHub org/user name.
-    projectName: 'OrigamiStudio', // Usually your repo name.
-    deploymentBranch: 'gh-pages',
-    trailingSlash: false,
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'kuaimeizheng', // Usually your GitHub org/user name.
+  projectName: 'OrigamiStudio', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
+  trailingSlash: false,
 
-    // Even if you don't use internalization, you can use this field to set useful
-    // metadata like html lang. For example, if your site is Chinese, you may want
-    // to replace "en" with "zh-Hans".
-    i18n: {
-        defaultLocale: 'en',
-        locales: ['en'],
-    },
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
 
-    presets: [
-        [
-            '@docusaurus/preset-classic',
-            ({
-                docs: {
-                    path: 'docs',
-                    routeBasePath: 'docs',
-                    sidebarPath: require.resolve('./sidebars.js'),
-                    // Please change this to your repo.
-                    // Remove this to remove the "edit this page" links.
-                    editUrl: 'https://github.com/kuaimeizheng/OrigamiStudio/tree/main/',
-                    showLastUpdateAuthor: true, // 显示最后编辑作者
-                    showLastUpdateTime: true, // 显示最后编辑时间
-                },
-                blog: {
-                    showReadingTime: true,
-                    // Please change this to your repo.
-                    // Remove this to remove the "edit this page" links.
-                    editUrl: 'https://github.com/kuaimeizheng/OrigamiStudio/tree/main/',
-                },
-                theme: {
-                    customCss: require.resolve('./src/css/custom.css'),
-                },
-            }),
-        ],
+
+
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      ({
+        docs: {
+          path: 'docs',
+          routeBasePath: 'docs',
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl: 'https://github.com/kuaimeizheng/OrigamiStudio/tree/main/',
+          showLastUpdateAuthor: true, // 显示最后编辑作者
+          showLastUpdateTime: true, // 显示最后编辑时间
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl: 'https://github.com/kuaimeizheng/OrigamiStudio/tree/main/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
     ],
-    plugins: [
-        [
-            '@docusaurus/plugin-content-docs',
-            {
-                id: 'learn',
-                path: 'learn',
-                routeBasePath: 'learn',
-                sidebarPath: require.resolve('./sidebars.js'),
-                editUrl: 'https://github.com/kuaimeizheng/OrigamiStudio/tree/main/',
-                showLastUpdateAuthor: true, // 显示最后编辑作者
-                showLastUpdateTime: true, // 显示最后编辑时间
-                // ... other options
-            },
-        ],
-        [
-            '@docusaurus/plugin-content-docs',
-            {
-                id: 'example',
-                path: 'example',
-                routeBasePath: 'example',
-                sidebarPath: require.resolve('./sidebars.js'),
-                editUrl: 'https://github.com/kuaimeizheng/OrigamiStudio/tree/main/',
-                showLastUpdateAuthor: true, // 显示最后编辑作者
-                showLastUpdateTime: true, // 显示最后编辑时间
-                // ... other options
-            },
-        ],
-
-
+  ],
+  /*plugins: [
+    [
+      '@docusaurus/plugin-content-docs',
+      {
+        id: 'documention',
+        path: 'documention',
+        routeBasePath: 'documention',
+        sidebarPath: require.resolve('./sidebars.js'),
+        editUrl: 'https://github.com/kuaimeizheng/OrigamiStudio/tree/main/',
+        showLastUpdateAuthor: true, // 显示最后编辑作者
+        showLastUpdateTime: true, // 显示最后编辑时间
+        // ... other options
+      },
     ],
+    [
+      '@docusaurus/plugin-content-docs',
+      {
+        id: 'example',
+        path: 'example',
+        routeBasePath: 'example',
+        sidebarPath: require.resolve('./sidebars.js'),
+        editUrl: 'https://github.com/kuaimeizheng/OrigamiStudio/tree/main/',
+        showLastUpdateAuthor: true, // 显示最后编辑作者
+        showLastUpdateTime: true, // 显示最后编辑时间
+        // ... other options
+      },
+    ],
+  ],*/
 
-    themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-        ({
+  themeConfig:
+  /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+      ({
+        metadata: [{
+          name: 'keywords',
+          content: 'origami, origami studio, prototype, 交互, 动效, 原型'
+        }],
+
         navbar: {
-            title: 'OrigamiStudio',
-            logo: {
-                alt: 'My Site Logo',
-                src: 'img/logo.svg',
+          title: 'OrigamiStudio',
+          logo: {
+            alt: 'My Site Logo',
+            src: 'img/logo.svg',
+          },
+
+          items: [
+            {
+              type: 'doc',
+              docId: 'documention/intro',
+              position: 'left',
+              label: '中文文档',
+              //className: 'emacs',
             },
-
-            items: [{
-                    type: 'doc',
-                    docId: 'intro',
-                    position: 'left',
-                    label: 'Origami 中文文档',
-                },
-
-                {
-                    to: '/learn/intro', // To highlight the navbar item, you must link to a document, not a top-level directory
-                    //type: 'doc',
-                    //docId: 'intro',
-                    position: 'left',
-                    label: '教程',
-                    activeBaseRegex: `docs/learn`,
-                },
-
-                {
-                    to: '/example/intro', // To highlight the navbar item, you must link to a document, not a top-level directory
-                    //type: 'doc',
-                    //docId: 'intro',
-                    position: 'left',
-                    label: '例子',
-                    activeBaseRegex: `docs/example`,
-                },
+            {
+              type: 'doc',
+              docId: 'example/intro',
+              position: 'left',
+              label: '例子',
+              //className: 'elisp',
+            },
+            /*{
+              type: 'doc',
+              docId: 'learn/intro',
+              position: 'left',
+              label: '教程',
+              //className: 'org',
+            },*/
 
 
-
-                {
-                    to: '/blog',
-                    label: '关于作者',
-                    position: 'left'
-                },
-                {
-                    href: 'https://github.com/kuaimeizheng/OrigamiStudio',
-                    label: 'GitHub',
-                    position: 'right',
-                },
-            ],
+            {
+              to: '/blog',
+              label: '关于作者',
+              position: 'left'
+            },
+            {
+              href: 'https://github.com/kuaimeizheng/OrigamiStudio',
+              label: 'GitHub',
+              position: 'right',
+            },
+          ],
         },
 
         footer: {
-            style: 'dark',
-            /*links: [
-              {
-                title: 'Docs',
-                items: [
-                  {
-                    label: 'Tutorial',
-                    to: '/docs/intro',
-                  },
-                ],
-              },
-              {
-                title: 'Community',
-                items: [
-                  {
-                    label: 'Stack Overflow',
-                    href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-                  },
-                  {
-                    label: 'Discord',
-                    href: 'https://discordapp.com/invite/docusaurus',
-                  },
-                  {
-                    label: 'Twitter',
-                    href: 'https://twitter.com/docusaurus',
-                  },
-                ],
-              },
-              {
-                title: 'More',
-                items: [
-                  {
-                    label: 'Blog',
-                    to: '/blog',
-                  },
-                  {
-                    label: 'GitHub',
-                    href: 'https://github.com/kuaimeizheng/OrigamiStudio',
-                  },
-                ],
-              },
-            ],*/
-            copyright: `Copyright © ${new Date().getFullYear()} Origami Studio, Personal. Built with kuaimeizheng.`,
+          style: 'dark',
+          /*links: [
+            {
+              title: 'Docs',
+              items: [
+                {
+                  label: 'Tutorial',
+                  to: '/docs/intro',
+                },
+              ],
+            },
+            {
+              title: 'Community',
+              items: [
+                {
+                  label: 'Stack Overflow',
+                  href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+                },
+                {
+                  label: 'Discord',
+                  href: 'https://discordapp.com/invite/docusaurus',
+                },
+                {
+                  label: 'Twitter',
+                  href: 'https://twitter.com/docusaurus',
+                },
+              ],
+            },
+            {
+              title: 'More',
+              items: [
+                {
+                  label: 'Blog',
+                  to: '/blog',
+                },
+                {
+                  label: 'GitHub',
+                  href: 'https://github.com/kuaimeizheng/OrigamiStudio',
+                },
+              ],
+            },
+          ],*/
+          copyright: `Copyright © ${new Date().getFullYear()} Origami Studio, Personal. Built with kuaimeizheng.`,
         },
         prism: {
-            theme: lightCodeTheme,
-            darkTheme: darkCodeTheme,
+          theme: lightCodeTheme,
+          darkTheme: darkCodeTheme,
         },
-    }),
+      }),
 };
 
 module.exports = config;
