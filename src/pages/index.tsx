bf194796d1548ea1c4eae3df9e7ce16e7a0df9bf
kuaimeizheng <kuaimeizheng@dingtalk.com>
2022-07-11 11:56:16 +0800

    add new content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/documentation/">
+            to="/docs/documentation/intro">
             点击查看文档
           </Link>
         </div>
