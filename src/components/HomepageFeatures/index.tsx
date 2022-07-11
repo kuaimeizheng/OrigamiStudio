a9bbd0777dc2204b9311a1c94fce0bc1d5a01690
kuaimeizheng <kuaimeizheng@dingtalk.com>
2022-07-11 11:36:02 +0800

    add new content

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -10,32 +10,29 @@ type FeatureItem = {
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Easy to Use',
+    title: '中文文档',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        中英文对照查阅。
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: '自定义模块',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        常用的自定义模块。
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: '自定义例子',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        常用的自定义例子。
       </>
     ),
   },
