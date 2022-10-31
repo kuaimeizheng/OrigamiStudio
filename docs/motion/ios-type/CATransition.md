47963a9f1ee87b0224a974af7fb944cd45ebf9df
kuaimeizheng <kuaimeizheng@gmail.com>
2022-10-31 20:45:02 +0800

    add ios motion

---
+++ docs/motion/ios-type/CATransition.md
@@ -0,0 +1,44 @@
+---
+title: CATransition
+sidebar_position: 10
+last_update:
+  author: 蒯美政
+---
+
+# CATransition
+
+提供图层状态之间动画转换的对象。
+
+[相关链接](https://developer.apple.com/documentation/quartzcore/catransition)
+
+---
+
+## CATransition
+
+<div className="patch-container">
+    <div className="patch processor">
+        <h3>CATransition</h3>
+        <ul className="inputs">
+            <li>Number<span>0</span></li>
+            <li>startProgress<span>0</span></li>
+            <li>endProgress<span>1</span></li>
+            <li>type<span>fade</span></li>
+            <li>subtype<span>nil</span></li>
+            <li>filter<span>100</span></li>
+        </ul>
+        <ul className="outputs">
+            <li>Value<span>0</span> </li>
+        </ul>
+    </div>
+</div>
+
+## 设计参数
+
+| 参数          | 说明                                                                                                                                                                                         |
+| :------------ | :------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- |
+| Number        | 一个数值，表示动画开始和结束。                                                                                                                                                               |
+| startProgress | 一个 0-1 的数值。                                                                                                                                                                            |
+| endProgress   | 一个 0-1 的数值。                                                                                                                                                                            |
+| type          | 预定于的转换类型，默认为 fade。 [Common Transition Type](https://developer.apple.com/documentation/quartzcore/catransition/common_transition_types)                                          |
+| subtype       | 指定一个可选子类型，指示预定义的基于运动的过渡的方向，默认为 nil。[Common Transition Subtypes](https://developer.apple.com/documentation/quartzcore/catransition/common_transition_subtypes) |
+| filter        | 提供过渡的可选核心图像过滤器对象，默认为 nil。                                                                                                                                               |
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,228 +10,234 @@ const npm2yarn = require('@docusaurus/remark-plugin-npm2yarn');
 //
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-    title: 'Origami 研习社',
-    tagline: 'Origami 中文文档&使用手册&教程',
-    url: 'https://kuaimeizheng.github.io',
-    baseUrl: '/OrigamiStudio/',
-    onBrokenLinks: 'throw',
-    onBrokenMarkdownLinks: 'warn',
-    favicon: 'img/favicon.ico',
+  title: 'Origami 研习社',
+  tagline: 'Origami 中文文档&使用手册&教程',
+  url: 'https://kuaimeizheng.github.io',
+  baseUrl: '/OrigamiStudio/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
 
-    // GitHub pages deployment config.
-    // If you aren't using GitHub pages, you don't need these.
-    organizationName: 'kuaimeizheng', // Usually your GitHub org/user name.
-    projectName: 'OrigamiStudio', // Usually your repo name.
-    deploymentBranch: 'gh-pages',
-    trailingSlash: false,
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'kuaimeizheng', // Usually your GitHub org/user name.
+  projectName: 'OrigamiStudio', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
+  trailingSlash: false,
 
-    // Even if you don't use internalization, you can use this field to set useful
-    // metadata like html lang. For example, if your site is Chinese, you may want
-    // to replace "en" with "zh-Hans".
-    i18n: {
-        defaultLocale: 'zh-Hans',
-        locales: ['zh-Hans'],
-    },
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'zh-Hans',
+    locales: ['zh-Hans'],
+  },
 
 
-    presets: [
-        [
-            '@docusaurus/preset-classic',
-            ({
-                docs: {
-                    path: 'docs',
-                    routeBasePath: 'docs',
-                    sidebarPath: require.resolve('./sidebars.js'),
-                    // Please change this to your repo.
-                    // Remove this to remove the "edit this page" links.
-                    editUrl: 'https://github.com/kuaimeizheng/OrigamiStudio/tree/main/',
-                    showLastUpdateAuthor: true, // 显示最后编辑作者
-                    showLastUpdateTime: true, // 显示最后编辑时间
-                    remarkPlugins: [
-                        [require('@docusaurus/remark-plugin-npm2yarn'), {sync: true}],
-                    ],
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      ({
+        docs: {
+          path: 'docs',
+          routeBasePath: 'docs',
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl: 'https://github.com/kuaimeizheng/OrigamiStudio/tree/main/',
+          showLastUpdateAuthor: true, // 显示最后编辑作者
+          showLastUpdateTime: true, // 显示最后编辑时间
+          remarkPlugins: [
+            [require('@docusaurus/remark-plugin-npm2yarn'), { sync: true }],
+          ],
 
-                },
-                //remarkPlugins: [math, [npm2yarn, {sync: true}]],
-                pages: {
-                    remarkPlugins: [require('@docusaurus/remark-plugin-npm2yarn')],
-                },
-                blog: {
-                    showReadingTime: true,
-                    // Please change this to your repo.
-                    // Remove this to remove the "edit this page" links.
-                    editUrl: 'https://github.com/kuaimeizheng/OrigamiStudio/tree/main/',
-                },
-                theme: {
-                  //customCss: require.resolve('./src/css/custom.css',),
-                  customCss: [require.resolve('./src/css/custom.scss')]
-              },
-            }),
-        ],
-    ],
-
-    themes: [
-      // ... Your other themes.
-      [
-        require.resolve("@easyops-cn/docusaurus-search-local"),
-        {
-          // ... Your options.
-          // `hashed` is recommended as long-term-cache of index file is possible.
-          hashed: true,
-          // For Docs using Chinese, The `language` is recommended to set to:
-          // ```
-          // language: ["en", "zh"],
-          // ```
         },
-      ],
+        //remarkPlugins: [math, [npm2yarn, {sync: true}]],
+        pages: {
+          remarkPlugins: [require('@docusaurus/remark-plugin-npm2yarn')],
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl: 'https://github.com/kuaimeizheng/OrigamiStudio/tree/main/',
+        },
+        theme: {
+          //customCss: require.resolve('./src/css/custom.css',),
+          customCss: [require.resolve('./src/css/custom.scss')]
+        },
+      }),
     ],
+  ],
 
-    plugins: [
-      'docusaurus-plugin-sass',
-        [
-            '@docusaurus/plugin-ideal-image',
-            {
-                quality: 70,
-                max: 1030, // max resized image's size.
-                min: 640, // min resized image's size. if original is lower, use that size.
-                steps: 2, // the max number of images generated between min and max (inclusive)
-                disableInDev: false,
-            },
-        ],
+  themes: [
+    // ... Your other themes.
+    [
+      require.resolve("@easyops-cn/docusaurus-search-local"),
+      {
+        // ... Your options.
+        // `hashed` is recommended as long-term-cache of index file is possible.
+        hashed: true,
+        // For Docs using Chinese, The `language` is recommended to set to:
+        // ```
+        // language: ["en", "zh"],
+        // ```
+      },
     ],
+  ],
 
-    themeConfig:
+  plugins: [
+    'docusaurus-plugin-sass',
+    [
+      '@docusaurus/plugin-ideal-image',
+      {
+        quality: 70,
+        max: 1030, // max resized image's size.
+        min: 640, // min resized image's size. if original is lower, use that size.
+        steps: 2, // the max number of images generated between min and max (inclusive)
+        disableInDev: false,
+      },
+    ],
+  ],
+
+  themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-        ({
-            metadata: [{
-                name: 'keywords',
-                content: 'origami, origami studio, prototype, 交互, 动效, 原型'
-            }],
-            tableOfContents: {
-                minHeadingLevel: 2,
-                maxHeadingLevel: 5,
-            },
-            navbar: {
-                title: 'OrigamiStudio',
-                logo: {
-                    alt: 'My Site Logo',
-                    src: 'img/logo.svg',
-                },
-                items: [
-                    {
-                        type: 'doc',
-                        docId: 'documentation/intro',
-                        position: 'left',
-                        label: '中文文档',
-                    },
-                    {
-                        type: 'doc',
-                        docId: 'patches/intro',
-                        position: 'left',
-                        label: '模块',
-                    },
+    ({
+      metadata: [{
+        name: 'keywords',
+        content: 'origami, origami studio, prototype, 交互, 动效, 原型'
+      }],
+      tableOfContents: {
+        minHeadingLevel: 2,
+        maxHeadingLevel: 5,
+      },
+      navbar: {
+        title: 'OrigamiStudio',
+        logo: {
+          alt: 'My Site Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'documentation/intro',
+            position: 'left',
+            label: '中文文档',
+          },
+          {
+            type: 'doc',
+            docId: 'patches/intro',
+            position: 'left',
+            label: '模块',
+          },
 
-                    {
-                        to: 'showcase',
-                        label: '例子(待完善)',
-                        position: 'left'
-                    },
-                    {
-                        type: 'doc',
-                        docId: 'system/intro',
-                        position: 'left',
-                        label: '组件系统',
-                    },
-                    {
-                        type: 'doc',
-                        docId: 'special/intro',
-                        position: 'left',
-                        label: '专题',
-                    },
+          {
+            to: 'showcase',
+            label: '例子(待完善)',
+            position: 'left'
+          },
+          {
+            type: 'doc',
+            docId: 'system/intro',
+            position: 'left',
+            label: '组件系统',
+          },
+          {
+            type: 'doc',
+            docId: 'special/intro',
+            position: 'left',
+            label: '专题',
+          },
 
-                    {
-                        type: 'doc',
-                        docId: 'device/intro',
-                        position: 'left',
-                        label: '设备',
-                    },
-                    {
-                      type: 'doc',
-                      docId: 'research/intro',
-                      position: 'left',
-                      label: '技术探索',
-                  },
+          {
+            type: 'doc',
+            docId: 'device/intro',
+            position: 'left',
+            label: '设备',
+          },
+          {
+            type: 'doc',
+            docId: 'research/intro',
+            position: 'left',
+            label: '技术探索',
+          },
+          {
+            type: 'doc',
+            docId: 'motion/intro',
+            position: 'left',
+            label: '动画模块',
+          },
 
-                    {
-                        type: 'doc',
-                        docId: 'shortcuts/shortcuts',
-                        position: 'left',
-                        label: '键盘快捷键',
-                    },
+          {
+            type: 'doc',
+            docId: 'shortcuts/shortcuts',
+            position: 'left',
+            label: '键盘快捷键',
+          },
 
-                    {
-                        to: '/blog',
-                        label: '关于作者',
-                        position: 'right'
-                    },
-                    {
-                        href: 'https://github.com/kuaimeizheng/OrigamiStudio',
-                        label: 'GitHub',
-                        position: 'right',
-                    },
-                ],
-            },
+          {
+            to: '/blog',
+            label: '关于作者',
+            position: 'right'
+          },
+          {
+            href: 'https://github.com/kuaimeizheng/OrigamiStudio',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
 
-            footer: {
-                style: 'dark',
-                /*links: [
-                  {
-                    title: 'Docs',
-                    items: [
-                      {
-                        label: 'Tutorial',
-                        to: '/docs/intro',
-                      },
-                    ],
-                  },
-                  {
-                    title: 'Community',
-                    items: [
-                      {
-                        label: 'Stack Overflow',
-                        href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-                      },
-                      {
-                        label: 'Discord',
-                        href: 'https://discordapp.com/invite/docusaurus',
-                      },
-                      {
-                        label: 'Twitter',
-                        href: 'https://twitter.com/docusaurus',
-                      },
-                    ],
-                  },
-                  {
-                    title: 'More',
-                    items: [
-                      {
-                        label: 'Blog',
-                        to: '/blog',
-                      },
-                      {
-                        label: 'GitHub',
-                        href: 'https://github.com/kuaimeizheng/OrigamiStudio',
-                      },
-                    ],
-                  },
-                ],*/
-                copyright: `Copyright © ${new Date().getFullYear()} Origami Studio, Personal. Built with kuaimeizheng.`,
-            },
-            prism: {
-                theme: lightCodeTheme,
-                darkTheme: darkCodeTheme,
-            },
-        }),
+      footer: {
+        style: 'dark',
+        /*links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Tutorial',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Stack Overflow',
+                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+              },
+              {
+                label: 'Discord',
+                href: 'https://discordapp.com/invite/docusaurus',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/docusaurus',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/kuaimeizheng/OrigamiStudio',
+              },
+            ],
+          },
+        ],*/
+        copyright: `Copyright © ${new Date().getFullYear()} Origami Studio, Personal. Built with kuaimeizheng.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
 };
 
 module.exports = config;
